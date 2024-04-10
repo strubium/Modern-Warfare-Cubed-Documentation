7ba1bd763ee53d5b9c355552b940966c203ee826
Desoroxxx <desoroxxx@gmail.com>
2024-04-10 17:41:12 +0200

    Warn broken links don't throw

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'Cubed-Development', // Usually your GitHub org/user name.
   projectName: 'Modern-Warfare-Cubed-Documentation', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
